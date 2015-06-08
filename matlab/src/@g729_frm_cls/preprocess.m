329d04829efd83bc1a6d1ac21a1a551b9633c5ed
asubbiah <asubbiah@scu.edu>
2015-06-08 15:56:31 -0700

    Added a gitignore file Restructured again the software directory

--- matlab/src/@g729_frm_cls/preprocess.m
+++
@@ -1,5 +0,0 @@
-%% This function is to preprocess the frame before starting the encoding procedure
-function obj = preprocess(obj) %% Function to split the frames into two different patterns
-	obj.cur_subfrm1 = obj.cur_frame(0*obj.SUB_FRM_SIZE+1:1*obj.SUB_FRM_SIZE);
-	obj.cur_subfrm2 = obj.cur_frame(1*obj.SUB_FRM_SIZE+1:2*obj.SUB_FRM_SIZE);
-end
